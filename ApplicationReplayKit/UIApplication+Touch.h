c8e9d6356cbf17b4cc6492aeef3b15e95648bba0
Zhiyang Fu <fuzhiyang0123@gmail.com>
2018-11-17 22:17:01 +0800

    fake touch

---
+++ ApplicationReplayKit/UIApplication+Touch.h
@@ -0,0 +1,13 @@
+//
+//  UIApplication+Touch.h
+//  ApplicationReplayKit
+//
+//  Created by Zhiyang Fu on 2018/11/17.
+//  Copyright © 2018 Zhiyang Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIApplication (Touch)
+
+@end
--- ApplicationReplayKit/ViewController.m
+++ ApplicationReplayKit/ViewController.m
@@ -7,10 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <JGMethodSwizzler.h>
+#import "PTFakeTouch.h"
 
 @interface ViewController () // <UITableViewDelegate, UITableViewDataSource>
 
-@property (nonatomic, weak) UIButton *button;
+@property (nonatomic, weak) UIButton *button1;
+
+@property (nonatomic, strong) NSMutableArray<UIEvent *> *events;
+
+@property (nonatomic, assign) BOOL endRecord;
 
 @end
 
@@ -19,8 +25,47 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.events = [NSMutableArray array];
+    
+//    UIApplication *application = [UIApplication sharedApplication];
+//
+//    __weak ViewController *weakThis = self;
+//    [application
+//     swizzleMethod:@selector(sendEvent:)
+//     withReplacement:JGMethodReplacementProviderBlock {
+//         return JGMethodReplacement(void, UIApplication *, UIEvent *event) {
+//             __strong __typeof(weakThis) strongThis = weakThis;
+//
+//             if (!strongThis.endRecord) {
+//                 NSLog(@"event: %@", event);
+//                 [strongThis.events addObject:event];
+//             }
+//             else {
+//                 NSLog(@"event: %@", event);
+//             }
+//
+//            JGOriginalImplementation(void, event);
+//         };
+//     }];
 }
 
+- (IBAction)button1Tapped:(id)sender
+{
+    NSLog(@"%@", @"button1 tapped");
+    
+    self.endRecord = YES;
+}
+
+- (IBAction)button2Tapped:(id)sender
+{
+//    for (UIEvent *event in self.events) {
+//        [[UIApplication sharedApplication] sendEvent:event];
+//    }
+    
+    NSInteger pointId = [PTFakeMetaTouch getAvailablePointId];
+    [PTFakeMetaTouch fakeTouchId:pointId AtPoint:CGPointMake(154.66665649414062,284.66665649414062) withTouchPhase:UITouchPhaseBegan];
+    [PTFakeMetaTouch fakeTouchId:pointId AtPoint:CGPointMake(154.66665649414062,284.66665649414062) withTouchPhase:UITouchPhaseEnded];
+}
 
 @end
